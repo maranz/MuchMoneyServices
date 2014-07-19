1e7764ffdd6db089922faa5786f026c145e765e3
maranz <luca.marani.78@gmail.com>
2014-07-19 16:25:59 +0200

    CrossDomain with JSONP

--- MuchMoneyDoorServices.php
+++ MuchMoneyDoorServices.php
@@ -1,43 +1,52 @@
-<?php    
+<?php
     include "config.php";
     include "helperSP.php";
     include "moneyout.php";
     include "users.php";
     include "itemcost.php";
     include "menu.php";
-    include "message.php";    
+    include "message.php";
     
-    $action = $_REQUEST['action'];
-    $userid = $_REQUEST['userid'];
-    $appid = $_REQUEST['appid'];
-   
     $msg = new message();
+         
+    $param = $_GET['param'];
+    if ($param == ''){
+        $msg->addError("Parametro 'param' non passato");
+        echo $msg->getJSON();
+        exit();
+    }
+    
+    $params = json_decode($param, true);
+        
+    $action = $params['action'];
+    $userid = $params['userid'];
+    $appid = $params['appid'];
      
     if ($action == ''){        
-        $msg->addError("Parametro 'action' non passato");    
+        $msg->addError("Parametro 'action' non passato");
         echo $msg->getJSON();
         exit();
     }    
      
     if ($userid == ''){        
-        $msg->addError("Parametro 'userid' non passato");    
+        $msg->addError("Parametro 'userid' non passato");
         echo $msg->getJSON();
         exit();
     }
     
     if ($appid == ''){        
-        $msg->addError("Parametro 'appid' non passato");    
+        $msg->addError("Parametro 'appid' non passato");
         echo $msg->getJSON();
         exit();
-    } 
-        
+    }
+    
     try
     {
-        $config = new config();    
+        $config = new config();
         $conn = new mysqli($config->DB_host, $config->DB_user, $config->DB_password, $config->DB_name);
              
         if (mysqli_connect_errno()) {
-            $msg->addError("Errore connessione al DBMS: ".mysqli_connect_error());    
+            $msg->addError("Errore connessione al DBMS: ".mysqli_connect_error());
             echo $msg->getJSON();
             exit();
         }
@@ -45,45 +54,45 @@
             $users = new users();
             $isValid = $users -> isValid($conn, $userid, $appid, $msg);
             if ($isValid != TRUE){
-                $msg->addError("Utente non trovato o non abilitato all'accesso");    
+                $msg->addError("Utente non trovato o non abilitato all'accesso");
                 echo $msg->getJSON();
                 exit();
             }
-            $rows = array();            
+            $rows = array();
             switch ($action) {
                 case "users":
                     $rows = users::getList($conn, $appid, $msg);
                     break;
-                case "startmenu":                    
+                case "startmenu":   
                     $list = new menu();
                     $rows = $list->getStartMenu($conn, $appid, $msg);
                     break;
                 case "insertmoneyout":
-                    $useridOwner = $_REQUEST['useridowner'];
+                    $useridOwner = $params['useridowner'];
                     if ($useridOwner == ''){        
-                        $msg->addError("Parametro 'useridOwner' non passato");    
+                        $msg->addError("Parametro 'useridOwner' non passato");
                         echo $msg->getJSON();
                         exit();
-                    }
-                    $itemcostname = $_REQUEST['itemcostname'];
+                    }                    
+                    $itemcostname = $params['itemcostname'];
                     if ($itemcostname == ''){        
-                        $msg->addError("Parametro 'itemcostname' non passato");    
+                        $msg->addError("Parametro 'itemcostname' non passato");
                         echo $msg->getJSON();
                         exit();
-                    }
-                    $vdate = $_REQUEST['vdate'];
+                    }                    
+                    $vdate = $params['vdate'];
                     if ($vdate == ''){        
-                        $msg->addError("Parametro 'vdate' non passato");    
+                        $msg->addError("Parametro 'vdate' non passato");
                         echo $msg->getJSON();
                         exit();
-                    }
-                    $money = $_REQUEST['money'];
+                    }                    
+                    $money = $params['money'];
                     if ($money == ''){        
-                        $msg->addError("Parametro 'money' non passato");    
+                        $msg->addError("Parametro 'money' non passato");
                         echo $msg->getJSON();
                         exit();
-                    }
-                    $groupid = $_REQUEST['groupid'];
+                    }                    
+                    $groupid = $params['groupid'];
                     if (trim($groupid) == ''){
                         $groupid = null;
                     }
@@ -97,30 +106,24 @@
                                             ,$appid
                                             ,$msg);
                 break;
-                case "itemcosts":
-                    $name = $_REQUEST['name'];
-                    $vdate = $_REQUEST['vdate'];
-                    
-                    $name="";
-                    $vdate="";
-                    
+                case "itemcosts":                    
+                    $name = $params['name'];
+                    $vdate = $params['vdate'];
                     $rows = itemcost::getList($conn
                                              ,$name
                                              ,$appid                                    
                                              ,$vdate
                                              ,$msg
-                                             ); 
+                                             );
                 break;
             }
-            
-            if (count($rows) > 0){
-                header("Content-type:application/json");
-                echo json_encode($rows);
-            }
-            else {
-                $msg->addError("not found element");
-                echo $msg->getJSON();
-            }    
+
+            if (count($rows) > 0){                
+                $msg->addResult($rows);
+            }else{
+                $msg->addError("'".$action."'"." not found element");
+            }             
+            echo $msg->getJSON();
         }
     } catch (Exception $e) {
        $msg->addError('Caught exception: ',  $e->getMessage(), "\n");
--- message.php
+++ message.php
@@ -1,23 +1,39 @@
 <?php
     class message
     {
-        public $list; 
+        public $errors;
+        public $result; 
         
-         function __construct() {
-             $list = array();
-         }
+        function __construct() {
+             $errors = array();
+             $result = array();
+        }
         
         public function addError($msg)
         {
             $item = array();
             $item['error'] = true;
             $item['msg'] = $msg;
-            $this->list[] = $item;
+            $this->errors[] = $item;
+        }
+        
+        public function addResult($rows)
+        {
+            $this->result = $rows;
         }
         
         public function getJSON()
         {
-            return json_encode($this->list);
+            $list = array();
+            if (count($this->errors) > 0){
+                $list = $this->errors;
+            }
+            else {
+                $list = $this->result;
+            }       
+            header("Content-Type: application/json; charset=utf-8");
+            $json = json_encode($list);
+            return isset($_GET['callback']) ? $_GET['callback'].'('.$json.')' : $json;
         }
     }
 ?>
